8386cdf4a69cbb993e2521e2b5d69324cf31ecd7
Gamm4a <gamael77c11@gmail.com>
2026-06-02 16:48:58 -0700

    Update HelloController.java

--- Verano 2026/WEB/MiPrimerServidorWEb/src/main/java/com/example/MiPrimerServidorWEb/HelloController.java
+++ Verano 2026/WEB/MiPrimerServidorWEb/src/main/java/com/example/MiPrimerServidorWEb/HelloController.java
@@ -7,17 +7,20 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api")
 public class HelloController {
 
+
+    //metodo get sin parametro
     @GetMapping("/hello")
     public String hello(){
         return "hola desde mi controlador Spring Boot";
     }
 
+    //metodo get
     @GetMapping("/saludo")
     public String saludo(@RequestParam String nombre){
         return "hola, "+ nombre +" este es mi servidor de aplicacion";
 
     }
-
+    //metodo post
     @PostMapping("/mensaje")
     public String recibirMensaje(@RequestBody String mensaje){
         return "Recibir tu mensaje "+ mensaje;
