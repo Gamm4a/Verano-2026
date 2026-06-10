993799f0afc45bda145b30bdb4b8ae07486d7bfa
Gamm4a <gamael77c11@gmail.com>
2026-06-10 14:20:26 -0700

    asignacion 7

---
+++ Verano 2026/WEB/Asignacion7/src/main/java/com/example/asignacion7/Asignacion7Application.java
@@ -0,0 +1,13 @@
+package com.example.asignacion7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Asignacion7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Asignacion7Application.class, args);
+    }
+
+}
---
+++ Verano 2026/WEB/Asignacion7/src/test/java/com/example/asignacion7/Asignacion7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.asignacion7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Asignacion7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
