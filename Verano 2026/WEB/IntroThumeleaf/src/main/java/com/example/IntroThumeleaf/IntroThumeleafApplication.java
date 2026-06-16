7cb593d4df51f7ae0d3eb34a235ddf62f6176c40
Gamm4a <gamael77c11@gmail.com>
2026-06-15 18:04:33 -0700

    IntroThumeleaf

---
+++ Verano 2026/WEB/IntroThumeleaf/src/main/java/com/example/IntroThumeleaf/IntroThumeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.IntroThumeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroThumeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroThumeleafApplication.class, args);
+	}
+
+}
---
+++ Verano 2026/WEB/IntroThumeleaf/src/test/java/com/example/IntroThumeleaf/IntroThumeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IntroThumeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroThumeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
